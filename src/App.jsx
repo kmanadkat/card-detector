a472fff9a8a7dd1c4591cfa433acce3f4f4f8923
Krupesh Anadkat <kmanadkat@gmail.com>
2021-09-03 11:14:58 +0530

    Added Author & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className='min-h-screen pt-40 pb-12 sm:pb-0 sm:pt-32 bg-[#DDEDFD] flex items-center justify-center px-6'>
       <CardDetector />
+      <p className='absolute bottom-2 text-gray-600 left-1/2 -translate-x-1/2'>Krupesh Anadkat</p>
     </div>
   )
 }
